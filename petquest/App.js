21c6beac8616caf01cc41f7efac1a3d143609744
pianelson <pianelson@knights.ucf.edu>
2020-08-27 21:45:11 -0400

    temp

--- petquest/App.js
+++ petquest/App.js
@@ -1,62 +1,32 @@
 import React from 'react';
 import { StyleSheet, Button, View, SafeAreaView, Text, Alert } from 'react-native';
-
 const Separator = () => (
   <View style={styles.separator} />
 );
 
 const App = () => (
   <SafeAreaView style={styles.container}>
-    <View>
-      <Text style={styles.title}>
-        The title and onPress handler are required. It is recommended to set accessibilityLabel to help make your app usable by everyone.
-      </Text>
-      <Button
-        title="Press me"
-        onPress={() => Alert.alert('Simple Button pressed')}
-      />
-    </View>
+
     <Separator />
     <View>
       <Text style={styles.title}>
-        Adjust the color in a way that looks standard on each platform. On  iOS, the color prop controls the color of the text. On Android, the color adjusts the background color of the button.
-      </Text>
+        Welcome back!      </Text>
       <Button
-        title="Press me"
-        color="#f194ff"
+        style={styles.button}
+        title="Log In"
         onPress={() => Alert.alert('Button with adjusted color pressed')}
       />
     </View>
     <Separator />
-    <View>
-      <Text style={styles.title}>
-        All interaction for the component are disabled.
-      </Text>
-      <Button
-        title="Press me"
-        disabled
-        onPress={() => Alert.alert('Cannot press this one')}
-      />
-    </View>
-    <Separator />
-    <View>
-      <Text style={styles.title}>
-        This layout strategy lets the title define the width of the button.
-      </Text>
-      <View style={styles.fixToText}>
-        <Button
-          title="Left button"
-          onPress={() => Alert.alert('Left button pressed')}
-        />
-        <Button
-          title="Right button"
-          onPress={() => Alert.alert('Right button pressed')}
-        />
-      </View>
-    </View>
+
   </SafeAreaView>
 );
 
+     /* <Button
+        title="Press me"
+        disabled
+        onPress={() => Alert.alert('Cannot press this one')}
+      /> */
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -73,8 +43,36 @@ const styles = StyleSheet.create({
   },
   separator: {
     marginVertical: 8,
-    borderBottomColor: '#737373',
+    borderBottomColor: '#F0AD4E',
     borderBottomWidth: StyleSheet.hairlineWidth,
+  },
+  inputText:{
+    height:50,
+    color:"white"
+  },
+  inputView:{
+    width:"80%",
+    backgroundColor:"#465881",
+    borderRadius:25,
+    height:50,
+    marginBottom:20,
+    justifyContent:"center",
+    padding:20
+  },
+  forgot:{
+    color:"peru",
+    fontSize:11
+  },
+  button:{
+    width:"80%",
+    backgroundColor:"#F0AD4E",
+    color:"#F0AD4E",
+    borderRadius:25,
+    height:50,
+    alignItems:"center",
+    justifyContent:"center",
+    marginTop:40,
+    marginBottom:10
   },
 });
 
